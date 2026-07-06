f03f655b08bb3693de79ae9ffdad54ea9233adf1
springtansy <elvis.blomster+github@gmail.com>
2026-07-06 16:58:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,53 +11,6 @@ let notes = false;
 let selectedSquare = null;
 const squares = [];
 
-class Square {
-    constructor(col, row) {
-        this.col = col;
-        this.row = row;
-
-        this.x = col * 60 + 2;
-        this.y = row * 60 + 2;
-
-        this.value = 0;
-        this.notes = [];
-        this.cage = -1;
-        this.selected = false;
-    }
-
-    draw(ctx) {
-        ctx.fillStyle = this.selected ? "#cfcfcf" : "#ffffff";
-        ctx.fillRect(this.x, this.y, 56, 56);
-        if (this.value !== 0) {
-            ctx.fillStyle = "#000000";
-            ctx.font = "bold 40px Verdana";
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.fillText(this.value,this.x+28,this.y+30);
-        } else {
-            ctx.fillStyle = "#000000";
-            ctx.font = "10px Verdana";
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            for (const note of this.notes) {
-                const xOffset = 19+((note-1)%3)*12;
-                const yOffset = 20+(Math.floor((note-1)/3))*12;
-                ctx.fillText(note,this.x+xOffset,this.y+yOffset);
-            }
-        }
-    }
-
-    toggleNote(key) {
-        const index = this.notes.indexOf(key);
-        
-        if (index !== -1) {
-            this.notes.splice(index, 1);
-        } else {
-            this.notes.push(key);
-        }
-    }
-}
-
 function selectSquare(col, row) {
     for (const square of squares) {
         square.selected = false;
