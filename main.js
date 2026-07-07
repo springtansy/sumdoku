2f04284c2e79918ac94c34cefffdafc22053c14d
springtansy <elvis.blomster+github@gmail.com>
2026-07-07 15:09:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,58 +6,47 @@ const ctx = canvas.getContext("2d");
 canvas.width = 540;
 canvas.height = 540;
 
+const board = new Board();
+
 let lastTime = null;
 let notes = false;
 let selectedSquare = null;
-const squares = [];
-
-function selectSquare(col, row) {
-    for (const square of squares) {
-        square.selected = false;
-    }
-
-    selectedSquare = squares.find(
-        s => s.col === col && s.row === row
-    );
-
-    if (selectedSquare) {
-        selectedSquare.selected = true;
-    }
-}
-
-for (let row = 0; row < 9; row++) {
-    for (let col = 0; col < 9; col++) {
-        squares.push(new Square(col, row));
-    }
-}
 
 document.addEventListener("keydown", (event) => {
+
     if (event.key.toLowerCase() === "n") {
         notes = !notes;
         return;
     }
+
     if (event.key >= "1" && event.key <= "9") {
+
         if (!selectedSquare) return;
+
         if (notes) {
             selectedSquare.toggleNote(Number(event.key));
-            return;
+        } else {
+            selectedSquare.value = Number(event.key);
+            selectedSquare.notes = [];
         }
-        selectedSquare.value = Number(event.key);
+
         return;
     }
+
     if (
         event.key === "0" ||
         event.key === "Backspace" ||
         event.key === "Delete"
     ) {
         if (!selectedSquare) return;
-        selectedSquare.notes = [];
-        selectedSquare.value = 0;
-        return;
+
+        selectedSquare.clear();
     }
+
 });
 
 canvas.addEventListener("mousedown", (event) => {
+
     const rect = canvas.getBoundingClientRect();
 
     const x = event.clientX - rect.left;
@@ -67,30 +56,29 @@ canvas.addEventListener("mousedown", (event) => {
     const row = Math.floor(y / 60);
 
     if (col >= 0 && col < 9 && row >= 0 && row < 9) {
-        selectSquare(col, row);
+        selectedSquare = board.select(col, row);
     }
-});
 
-function drawSquares(ctx) {
-  for (const square of squares) {
-    square.draw(ctx)
-  }
-}
+});
 
 function clamp(value, min, max) {
     return Math.max(min, Math.min(max, value));
 }
 
 function update(dt) {
-    
+
 }
 
 function draw(ctx) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSquares(ctx);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    board.draw(ctx);
+
 }
 
 function gameLoop(timestamp) {
+
     if (lastTime === null) {
         lastTime = timestamp;
     }
